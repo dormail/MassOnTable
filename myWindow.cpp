886ecd4dba3a7121b80a0bb2857ad7b647f1f8fd
Matthias Maile <matthias.maile1@gmail.com>
2020-08-01 10:46:06 +0200

    some setting added, but not linked yet

--- myWindow.cpp
+++ myWindow.cpp
@@ -3,16 +3,59 @@
 #include "myWindow.h"
 
 MyWindow::MyWindow(unsigned int sizeX, unsigned int sizeY)
-	: dr(), hBoxMaster(false, 30)
+	: dr(), 
+	/* setting master boxes */
+	hBoxMaster(false, 30),
+	vBoxSettings(false, 10),
+	/* setting small boxes for each setting */
+	boxM1(false, 10),
+	boxM2(false, 10),
+	boxl(false, 10),
+	boxL(false, 10),
+	boxG(false, 10),
+	/* setting buttons */
+	buttonM1(Gtk::Adjustment::create(1, 0.001, 1000, 0.5, 1, 42), 0, 1),
+	/* setting labels for the buttons */
+	labelM1("Mass 1 = "),
+	labelM2("Mass 2 = "),
+	labell("l = "),
+	labelL("L = "),
+	labelG("g = "),
+	/* setting units */
+	unitM1("kg"),
+	unitM2("kg"),
+	unitlength("m"),
+	unitG("m/s²"),
+	unitL("kg m²/s")
 {
 	set_title("Mass on table");
 	set_default_size(sizeX, sizeY);
 	set_border_width(10);
 
 	/* add all the gtk stuff together */
+	/* Global HBox, Settings and Drawing area */
 	add(hBoxMaster);
 	mainFrame.add(dr);
 	hBoxMaster.pack_start(mainFrame);
+	hBoxMaster.pack_start(vBoxSettings, Gtk::PackOptions::PACK_SHRINK);
+
+	/* adding settings to the setting VBox */
+	/* m1 */
+	boxM1.add(labelM1);
+	boxM1.add(buttonM1);
+	boxM1.add(unitM1);
+	vBoxSettings.pack_start(boxM1, Gtk::PackOptions::PACK_SHRINK);
+	/* m2 */
+	boxM2.add(labelM2);
+	boxM2.add(buttonM2);
+	boxM2.add(unitM2);
+	vBoxSettings.pack_start(boxM2, Gtk::PackOptions::PACK_SHRINK);
+
+	/* L */
+	boxL.add(labelL);
+	boxL.add(buttonL);
+	boxL.add(unitL);
+	vBoxSettings.pack_start(boxL, Gtk::PackOptions::PACK_SHRINK);
 	
 	show_all_children();
 }
--- myWindow.h
+++ myWindow.h
@@ -20,7 +20,23 @@ class MyWindow : public Gtk::Window {
 		sigc::connection timeout;
 
 		/* gtk stuff */
+		// HBox containing everything
 		Gtk::HBox hBoxMaster;
+		// VBox with all the settings
+		Gtk::VBox vBoxSettings;
+		// HBox for each setting
+		Gtk::HBox boxM1, boxM2, boxl, boxL, boxG;
+		/* spin button for typing in values for m1, etc.
+		 * Labels so that user knows what he is typing in
+		 * Alignment for looks
+		 * Energery box giving out energy
+		 */
+		Gtk::SpinButton buttonM1, buttonM2, buttonl, buttonL, buttonG;
+		Gtk::Label labelM1, labelM2, labell, labelL, labelG;
+		Gtk::Label unitM1, unitM2, unitlength, unitG, unitL;
+		Gtk::Alignment buttonAlign;
+		Gtk::HBox hBoxEnergy;
+		Gtk::Label totalEnergy;
 
 	public:
 		/* constructor and destructor */
